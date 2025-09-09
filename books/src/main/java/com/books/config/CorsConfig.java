bdaa85e861b2fb313ce9685cf12bcbca65f120ab
anisort <jaki.anisort@gmail.com>
2025-09-09 16:56:56 +0300

    fix cors config in books

--- books/src/main/java/com/books/config/CorsConfig.java
+++ books/src/main/java/com/books/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "http://frontend:4200")
+                        .allowedOrigins("http://localhost:4200", "http://frontend:4200", "http://localhost")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
