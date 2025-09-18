d461772879f50d9da6d97d68a63ae3ab886593fe
anisort <jaki.anisort@gmail.com>
2025-09-18 16:46:08 +0300

    add controllers for chats and messages

---
+++ assistant/src/main/java/com/ai/books/assistant/controllers/ChatsController.java
@@ -0,0 +1,46 @@
+package com.ai.books.assistant.controllers;
+
+import com.ai.books.assistant.dto.ChatDto;
+import com.ai.books.assistant.services.chat.ChatsService;
+import com.ai.books.assistant.services.chat.IChatsService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/chats")
+public class ChatsController {
+
+    private final IChatsService chatsService;
+
+    public ChatsController(ChatsService chatsService) {
+        this.chatsService = chatsService;
+    }
+
+    @GetMapping
+    public List<ChatDto> getChats(@AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        return chatsService.getChats(userId);
+    }
+
+    @PostMapping
+    public ChatDto createChat(@AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        return chatsService.createChat(userId);
+    }
+
+    @PatchMapping("/{chatId}")
+    public ChatDto changeChatTitle(@PathVariable Long chatId, @RequestBody Map<String, String> title, @AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        return chatsService.changeChatTitle(chatId, userId, title.get("title"));
+    }
+
+    @DeleteMapping("/{chatId}")
+    public void deleteChat(@PathVariable Long chatId, @AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        chatsService.deleteChat(chatId, userId);
+    }
+}
---
+++ assistant/src/main/java/com/ai/books/assistant/controllers/MessagesController.java
@@ -0,0 +1,40 @@
+package com.ai.books.assistant.controllers;
+
+import com.ai.books.assistant.dto.MessageDto;
+import com.ai.books.assistant.services.messages.IMessagesService;
+import com.ai.books.assistant.services.messages.MessagesService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessagesController {
+
+    private final IMessagesService messagesService;
+
+    public MessagesController(MessagesService messagesService) {
+        this.messagesService = messagesService;
+    }
+
+    @GetMapping("{chatId}")
+    public List<MessageDto> getMessages(@PathVariable Long chatId, @AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        return messagesService.getMessages(chatId, userId);
+    }
+
+    @PostMapping("{chatId}")
+    public MessageDto sendMessage(@PathVariable Long chatId, @RequestBody Map<String, String> prompt, @AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        return messagesService.sendMessage(prompt.get("prompt"), chatId, userId);
+    }
+
+    @DeleteMapping("{userPromptId}")
+    public void deleteMessage(@PathVariable Long userPromptId, @AuthenticationPrincipal Jwt jwt) {
+        Long userId = Long.valueOf(jwt.getSubject());
+        messagesService.deleteMessage(userPromptId, userId);
+    }
+}
