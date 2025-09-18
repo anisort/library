56ec940b6eb4e64137839c469efdd0aa5c7b8262
anisort <jaki.anisort@gmail.com>
2025-09-18 16:55:31 +0300

    feat(repo): add repository methods for fetching by chat and ownership

--- assistant/src/main/java/com/ai/books/assistant/repositories/AssistantResponseRepository.java
+++ assistant/src/main/java/com/ai/books/assistant/repositories/AssistantResponseRepository.java
@@ -4,6 +4,9 @@ import com.ai.books.assistant.entities.AssistantResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AssistantResponseRepository extends JpaRepository<AssistantResponse, Long> {
+    Optional<AssistantResponse> findByUserPromptId(Long userPromptId);
 }
--- assistant/src/main/java/com/ai/books/assistant/repositories/UserPromptRepository.java
+++ assistant/src/main/java/com/ai/books/assistant/repositories/UserPromptRepository.java
@@ -4,6 +4,11 @@ import com.ai.books.assistant.entities.UserPrompt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserPromptRepository extends JpaRepository <UserPrompt, Long>{
+    List<UserPrompt> findAllByChatId(Long chatId);
+    Optional<UserPrompt> findByIdAndChat_UserId(Long userPromptId, Long userId);
 }
