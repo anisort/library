ad1da9c1d10443706185442b606186d4ae4d1ffe
anisort <jaki.anisort@gmail.com>
2025-09-22 16:54:59 +0300

    A new API URL for the assistant has been added to the environment configuration file.

--- frontend/src/enviroments/environment.ts
+++ frontend/src/enviroments/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   apiUrlAuth: 'http://localhost/auth/api',
   apiUrlBooks: 'http://localhost/books/api',
+  apiUrlAssistant: 'http://localhost/assistant/api',
   oauthClientId: 'auth-client',
   oauthIssuer: 'http://localhost/auth/',
   oauthSecret: 'secret'
